e8e5c2f5a478aca9e1feef2818fd0eae95cd1b0b
Gastón Carreño <gastonexc@gmail.com>
2021-10-28 21:34:47 -0300

    Reparación del botón mobile con js

--- js/navBarMobile.js
+++ js/navBarMobile.js
@@ -1,9 +1,11 @@
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".openMenu");
 const closeMenuBtn = document.querySelector(".closeMenu");
+const profileBtn = document.querySelector(".profileBtn");
 
 function toggleMenu(){
     menu.classList.toggle("menuOpened");
+    profileBtn.classList.toggle("menuOpenedButton")
 }
 
 openMenuBtn.addEventListener("click",toggleMenu);
